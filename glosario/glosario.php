53164f1330dcd39287a1af17be6f7c1e4a63c8c1
PedroFilho <psfilho81@hotmail.com>
2023-01-15 16:10:20 -0300

    Atualização

--- glosario/glosario.php
+++ glosario/glosario.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Glosário PHP</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1 align='center'>Glosário PHP</h1>
-    <hr>
+<?php echo '1'; ?>
+<?php echo '2'; ?>_
+<?php echo '3'; ?>
+_<?php echo '4'; ?>_<?php echo '5'; ?>
 
 </body>
 </html>
