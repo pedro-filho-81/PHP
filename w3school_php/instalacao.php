cf00882759d07a946f5eb1a5e79a7fcfedabbd7a
PedroFilho <psfilho81@hotmail.com>
2023-01-27 20:04:15 -0300

    Create instalacao.php

---
+++ w3school_php/instalacao.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Instalação do PHP</title>
+   <style>
+                body {
+            background-color: blue ;
+            color: white;
+            font-size: 13pt;
+        }
+        h1 {
+            text-align: center;
+        }
+        mark {
+            background-color: white;
+            color: red;
+        }
+        strong {
+            background-color: white;
+            color: black;
+        }
+        pre {
+        background-color: whitesmoke;
+        color: blue;
+        font-size: 13pt;
+        }
+    </style>
+</head>
+<body>
+   <h1>Instalação PHP</h1>
+
+   <h2>O que eu preciso?</h2>
+
+   <p>Para começar a usar o PHP, você pode:</p>
+   <pre>
+      Encontre um host com suporte a PHP e MySQL
+      Instale um servidor web no seu próprio PC e, 
+      em seguida, instale o PHP e o MySQL
+   </pre>
+   <hr>
+
+   <h2>Use um host da Web com suporte a PHP</h2>
+
+   <p>Se o seu servidor ativou o suporte para PHP, você não precisa fazer qualquer coisa.</p>
+
+   <p>Basta criar alguns arquivos, colocá-los em seu diretório da web e no servidor irá analisá-los automaticamente para você..php</p>
+
+   <p>Você não precisa compilar nada ou instalar ferramentas extras.</p>
+
+   <p>Como o PHP é gratuito, a maioria dos hosts da Web oferecer suporte a PHP.</p>
+   <hr>
+
+   <h2>Configure o PHP no seu próprio PC</h2>
+
+   <p>No entanto, se o seu servidor não suporta PHP, você deve:</p>
+   <pre>
+      instalar um servidor Web
+      instalar o PHP
+      instalar um banco de dados, como o MySQL
+   </pre>
+
+   <p>O site oficial do PHP (PHP.net) tem instruções de instalação para PHP:</p>
+   <pre>
+      <a href="http//php.net/manual/en/install.php">http//php.net/manual/en/install.php</a>
+   </pre>
+   <hr>
+   
+</body>
+</html>
