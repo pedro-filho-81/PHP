0cbe7993554a9c05e3c18d4b4deeb9235a24f19b
PedroFilho <psfilho81@hotmail.com>
2023-02-10 18:46:37 -0300

    Create introducao.php

---
+++ introducaoPHP/introducao.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Linguagem de Programação PHP</title>
+</head>
+<body>
+   <h1>Linguagem de Programação PHP</h1>
+   <h2>O que é PHP?</h2>
+   
+   <ul>
+      <li>PHP significa Hypertext Preprocessor.</li>
+      <li>PHP é uma linguagem interpretada, ou seja, não há necessidade de compilação.</li>
+      <li>PHP é uma linguagem de script de código aberto amplamente utilizada</li>
+      <li>Os scripts PHP são executados no servidor</li>
+      <li>PHP pode ser incorporado em HTML.</li>
+      <li>PHP é gratuito para baixar e usar</li>
+      <li>PHP é uma linguagem orientada a objetos.</li>
+      <li>PHP é uma linguagem simples e fácil de aprender.</li>
+   </ul>
+   <hr>
+
+   <h2>PHP é uma linguagem incrível e popular!</h2>
+   <p>É poderoso o suficiente para estar no centro do maior sistema de blogs na web (WordPress)!</p>
+   <p>É profundo o suficiente para executar grandes redes sociais!</p>
+   <hr>
+
+   <h2>Por que usar PHP</h2>
+
+   <p>PHP é uma linguagem de script do lado do servidor, que é usada para projetar os aplicativos web dinâmicos com o banco de dados MySQL.</p>
+
+   <ul>
+      <li>Ele lida com conteúdo dinâmico, banco de dados, bem como rastreamento de sessão para o site.</li>
+      <li>Você pode criar sessões em PHP.</li>
+      <li>Ele pode acessar cookies variáveis e também definir cookies.</li>
+      <li>Isso ajuda a criptografar os dados e aplicar a validação.</li>
+      <li>PHP suporta vários protocolos, como HTTP, POP3, SNMP, LDAP, IMAP, e muitos mais.</li>
+      <li>Usando a linguagem PHP, você pode controlar o usuário para acessar algumas páginas do seu site.</li>
+      <li>Como o PHP é fácil de instalar e configurar, esta é a principal razão pela qual o PHP é a melhor linguagem para aprender.</li>
+      <li>O PHP pode manipular os formulários, como - coletar os dados dos usuários usando formulários, salvá-los no banco de dados e retornar informações úteis ao usuário.</li>
+      <li>Por exemplo - Formulário de inscrição.</li>
+   </ul>
+   <hr>
+
+
+   O que é um arquivo PHP?
+   Os arquivos PHP podem conter texto, HTML, CSS, JavaScript e código PHP
+   O código PHP é executado no servidor e o resultado é retornado ao navegador como HTML simples
+   Arquivos PHP têm extensão ".php"
+   O que o PHP pode fazer?
+   PHP pode gerar conteúdo de página dinâmica
+   PHP pode criar, abrir, ler, escrever, excluir e fechar arquivos no servidor
+   PHP pode coletar dados de formulário
+   PHP pode enviar e receber cookies
+   PHP pode adicionar, excluir, modificar dados em seu banco de dados
+   PHP pode ser usado para controlar o acesso do usuário
+   PHP pode criptografar dados
+   Com o PHP você não está limitado a HTML de saída. Você pode produzir imagens ou PDF limas. Você também pode produzir qualquer texto, como XHTML e .XML.
+
+   ANÚNCIO
+
+   Por que PHP?
+   PHP roda em várias plataformas (Windows, Linux, Unix, Mac OS X, etc.)
+   PHP é compatível com quase todos os servidores usados hoje (Apache, IIS, etc.)
+   PHP suporta uma ampla gama de bancos de dados
+   PHP é gratuito. Faça o download a partir do recurso oficial do PHP: www.php.net
+   PHP é fácil de aprender e é executado de forma eficiente no lado do servidor
+   O que há de novo no PHP 7
+   PHP 7 é muito mais rápido do que a versão estável popular anterior (PHP 5.6)
+   PHP 7 melhorou o Tratamento de Erros
+   PHP 7 suporta declarações de tipo mais rigorosas para argumentos de função
+   PHP 7 suporta novos operadores (como o operador da nave espacial: <=>)
+</body>
+</html>
