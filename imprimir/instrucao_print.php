e493e9995b27a3b609a3b55a88cb79ba7bf1eea9
PedroFilho <psfilho81@hotmail.com>
2023-02-14 16:18:07 -0300

    Atualização

--- imprimir/instrucao_print.php
+++ imprimir/instrucao_print.php
@@ -5,7 +5,9 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Instrução print PHP</title>
-</head>
+   <link rel="stylesheet" href="style.css">
+
+</head> 
 <body>
          
    <?php
